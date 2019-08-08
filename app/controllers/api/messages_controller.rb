3d996fbeb1985c87334156d6587f44fe0a00e6b1
JUN1113 <dunkelrots@gmail.com>
2019-08-08 15:15:43 +0900

    change find_by to find

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,6 +1,6 @@
 class Api::MessagesController < ApplicationController
   def index
-    @group = Group.find_by(id: params[:group_id])
+    @group = Group.find(params[:group_id])
     @messages = @group.messages.where("id > #{params[:id]}")
   end
 end
